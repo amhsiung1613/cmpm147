53461150a5d3e729a7c6244062ff8650db1b4df4
amhsiung1613 <133808504+amhsiung1613@users.noreply.github.com>
2024-04-06 22:54:58 -0700

    hopefully this works?

--- experiment1/js/project.js
+++ experiment1/js/project.js
@@ -1,34 +1,117 @@
 // project.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Amber
+// Date: 4/6/2024
 
 // NOTE: This is how we might start a basic JavaaScript OOP project
 
 // Constants - User-servicable parts
 // In a longer project I like to put these in a separate file
 
-// define a class
-class MyProjectClass {
-  // constructor function
-  constructor(param1, param2) {
-    // set properties using 'this' keyword
-    this.property1 = param1;
-    this.property2 = param2;
-  }
-  
-  // define a method
-  myMethod() {
-    // code to run when method is called
-  }
-}
 
 function main() {
-  // create an instance of the class
-  let myInstance = new MyProjectClass("value1", "value2");
-
-  // call a method on the instance
-  myInstance.myMethod();
+  const fillers = {
+    starters: [
+      "in my opinion, $celeberties is $adjective",
+      "according to their start sign and moon phase, $celeberties is  $adjective",
+      "$celeberties is too $adjective2",
+      "have you heard, $celeberties is so $adjective2"
+    ],
+    celeberties: [
+      "Obama",
+      "Hellen Keller",
+      "Ronald Reagan",
+      "Robert Dinero",
+      "John Cena",
+      "Bill Hader",
+      "Andrew Garfield",
+      "Emma Watson",
+      "Robin Williams",
+      "Kristen Stewart",
+      "Toby Mcguire",
+      "Madonna",
+      "Tupac",
+    ],
+    adjective: [
+      "overrated",
+      "underwhelming",
+      "disappointing",
+      "spiteful",
+      "hateful",
+      "ignorant",
+      "idiotic",
+      "prissy",
+      "positive",
+      "mid",
+    ],
+    verb: [
+      "eat",
+      "sleep",
+      "think",
+      "talk",
+      "express themselves",
+      "jump",
+      "skip",
+      "cook",
+      "paint",
+      "sweep",
+      "act",
+    ],
+    adjective2: [
+      "silly",
+      "funny",
+      "dirty",
+      "clean",
+      "sweet",
+      "outrageous",
+      "confusing",
+      "annoying",
+      "gross",
+      "exhausting",
+      "frustrating",
+      "cringey",
+      
+    ],
+   
+  };
+  
+  const template = `$starters, there's something about the way they $verb, it's just so $adjective2.
+  
+  
+  `;
+  
+  
+  
+  // STUDENTS: You don't need to edit code below this line.
+  
+  const slotPattern = /\$(\w+)/;
+  
+  function replacer(match, name) {
+    let options = fillers[name];
+    if (options) {
+      return options[Math.floor(Math.random() * options.length)];
+    } else {
+      return `<UNKNOWN:${name}>`;
+    }
+  }
+  
+  
+  function generate() {
+    let story = template;  
+    while (story.match(slotPattern)) {
+      story = story.replace(slotPattern, replacer);
+    }
+    
+  
+    /* global box */
+    box.innerText = story;
+  }
+  
+  /* global clicker */
+  clicker.onclick = generate;
+  
+  generate();
+  
 }
 
 // let's get this party started - uncomment me
-//main();
+main();
